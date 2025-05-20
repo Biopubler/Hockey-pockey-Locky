ce5e57a9c6979c894dc7e94cc996dd55983a9a03
Tor Karlsson <tor.karlsson@skola.taby.se>
2025-05-20 09:52:39 +0200

    Försökte fixa knappens funktionalitet

---
+++ Hockey'Blaze.js
@@ -0,0 +1,8 @@
+
+const Knaprardet = document.getElementById("Knaprardet");
+const Titelkort = document.getElementById("Titelkort");
+
+Knaprardet.addEventListener("click", event =>{
+
+  Titelkort.style.display = "none";
+});
